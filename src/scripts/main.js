24478ba5c03594ef26caf3ee2a98569189886c3a
edvan silva <edvansilvapereira@gmail.com>
2023-05-15 11:51:45 -0300

    finalizando 2 secoes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question]');//aqui e para pegar os elemento para fazer o acoordeon
 
     for (let i = 0; i < buttons.length; i++) {
 
@@ -15,7 +15,19 @@ document.addEventListener("DOMContentLoaded", function(){
                 })
             
     }
+    for(let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', abreOuFechaResposta);
+    }
 })
+function abreOuFechaResposta(e){
+    const classe ='faq__questions__item--is-open';
+
+    const elementoPai = e.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+
+
+}
 
 function removeActiveTab() {
     const buttons = document.querySelectorAll('[data-tab-button]');
